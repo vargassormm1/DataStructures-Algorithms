12fba08c6b3cfae57d5002fcc8de4c69a1159529
mattvargas <vargassormm1@montclair.edu>
2021-12-14 14:42:21 -0500

    bubble sort algorithm

---
+++ SortingAlgorithms/BubbleSort.js
@@ -0,0 +1,19 @@
+const bubbleSort = (arr) => {
+  let noSwaps;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) {
+      break;
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSort([5, 1, 3, 2, 4, 6]));
