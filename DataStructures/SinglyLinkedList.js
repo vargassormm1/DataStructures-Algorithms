ddd3863e2eb1994e77e282bf28411403a3489721
mattvargas <vargassormm1@montclair.edu>
2021-12-22 19:10:27 -0500

    push method on singly linked node

--- DataStructures/SinglyLinkedList.js
+++ DataStructures/SinglyLinkedList.js
@@ -5,13 +5,42 @@ class Node {
   }
 }
 
-let first = new Node("Hello");
-first.next = new Node("How");
-first.next.next = new Node("Are");
-
-console.log(first);
+// let first = new Node("Hello");
+// first.next = new Node("How");
+// first.next.next = new Node("Are");
 
 // Node {
 //     val: 'Hello',
 //     next: Node { val: 'How', next: Node { val: 'Are', next: null } }
+//   }
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+// let list = new SinglyLinkedList();
+// list.push("Hello");
+// list.push("Matt");
+// list.push(20);
+// console.log(list);
+
+// SinglyLinkedList {
+//     head: Node { val: 'Hello', next: Node { val: 'Matt', next: [Node] } },
+//     tail: Node { val: 20, next: null },
+//     length: 3
 //   }
