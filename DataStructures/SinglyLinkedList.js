e75abd9973b2df7a8b77f638a598365292c5debf
mattvargas <vargassormm1@montclair.edu>
2021-12-25 22:17:38 -0500

    shifting method

--- DataStructures/SinglyLinkedList.js
+++ DataStructures/SinglyLinkedList.js
@@ -43,4 +43,16 @@ class SinglyLinkedList {
     }
     return current;
   }
+
+  shift() {
+    if (!this.head) return undefined;
+
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
 }
