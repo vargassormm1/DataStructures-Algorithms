3c1e53b81b3f603c73c27c14158cb1c8f137764c
mattvargas <vargassormm1@montclair.edu>
2022-01-06 23:53:20 -0500

    Breadth First Search

--- DataStructures/BinarySearchTree.js
+++ DataStructures/BinarySearchTree.js
@@ -55,4 +55,19 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+
+  BFS() {
+    let data = [];
+    let queue = [];
+    let node = this.root;
+
+    queue.push(this.root);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node);
+      if (node.left) queue.push(node.lef);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
 }
