6ed03bbf62a55402068d2dfffd31ae0e21bd8199
mattvargas <vargassormm1@montclair.edu>
2022-01-07 00:28:00 -0500

    DFS PreOrder PostOrder adn InOrder

--- DataStructures/BinarySearchTree.js
+++ DataStructures/BinarySearchTree.js
@@ -70,4 +70,43 @@ class BinarySearchTree {
     }
     return data;
   }
+
+  DFSPreOrder() {
+    let data = [];
+    let current = this.root;
+
+    function traverse(node) {
+      data.push(node);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(current);
+    return data;
+  }
+
+  DFSPostOrder() {
+    let data = [];
+    let current = this.root;
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node);
+    }
+    traverse(current);
+    return data;
+  }
+
+  DFSInOrder() {
+    let data = [];
+    let current = this.root;
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node);
+      if (node.right) traverse(node.right);
+    }
+    traverse(current);
+    return data;
+  }
 }
